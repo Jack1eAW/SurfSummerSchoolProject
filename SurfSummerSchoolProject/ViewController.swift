75baaf99757156cf73111dd686a408c3913eeefb
Jack1eAW <alcheb2014@gmail.com>
2022-08-02 16:46:36 +0300

    Initial Commit

---
+++ SurfSummerSchoolProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurfSummerSchoolProject
+//
+//  Created by Aleksandr Chebotarev on 8/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
